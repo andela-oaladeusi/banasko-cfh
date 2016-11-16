e83c6858b05830ba9ddd7ee783f676ab2a3f2c6c
Shalom Ayidu <shalom.ayidu@andela.com>
2016-11-16 13:10:52 +0100

    chore(coveralls): setup code coverage using coveralls.io

--- config/env/all.js
+++ config/env/all.js
@@ -6,5 +6,5 @@ var testLocal = 'mongodb://localhost/cfh';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-     db: process.env.MONGOHQ_URL || testLocal
+        db: process.env.MONGOHQ_URL || testLocal
 };
