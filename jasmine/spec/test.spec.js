7096a2d5fbb5ee2178aa6134ea6c2a6f1d73b740
enaho murphy <enahomurphy@gmail.com>
2016-11-16 12:36:46 +0100

    chore(front-end testing): it should fix extra space it test

--- jasmine/spec/test.spec.js
+++ jasmine/spec/test.spec.js
@@ -1,7 +1,5 @@
 function sqrt(x) {
-<<<<<<< HEAD
   return x * x;
-
 }
 
 describe('simple test ' , function() {
@@ -9,16 +7,4 @@ describe('simple test ' , function() {
     var root = sqrt(4);
     expect(root).toEqual(16);
   });
-=======
-  return x * x
 }
-
-describe('simple test ' , function() {
-
-  it('should return sqrt ', function() {
-    var root = sqrt(4)
-    expect(root).toEqual(16)
-  })
-
->>>>>>> 9ac67ed... chore( front-end testing): environment setup adding karma and jasmine for unit testing Finishes #134249123
-})
