4a16dc66b04ca608b8393851ed0d00f44fac7bff
enaho murphy <enahomurphy@gmail.com>
2016-11-16 11:18:52 +0100

    chore(houndCi error) fix semicolon error for houndCi

--- jasmine/spec/test.spec.js
+++ jasmine/spec/test.spec.js
@@ -1,12 +1,12 @@
 function sqrt(x) {
-  return x * x
-}
+  return x * x;
+};
 
 describe('simple test ' , function() {
 
   it('should return sqrt ', function() {
-    var root = sqrt(4)
-    expect(root).toEqual(16)
-  })
+    var root = sqrt(4);
+    expect(root).toEqual(16);
+  });
 
-})
+});
