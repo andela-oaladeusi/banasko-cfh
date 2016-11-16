c0a2d050f62825cdc1817dc1407f5e54085f6071
enaho murphy <enahomurphy@gmail.com>
2016-11-16 18:30:19 +0100

    fix merge conflict

--- jasmine/spec/test.spec.js
+++ jasmine/spec/test.spec.js
@@ -1,10 +1,13 @@
 function sqrt(x) {
   return x * x;
+
 }
 
 describe('simple test ' , function() {
   it('should return sqrt ', function() {
+
     var root = sqrt(4);
     expect(root).toEqual(16);
   });
+
 })
