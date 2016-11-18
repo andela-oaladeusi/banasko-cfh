f902e66f836d893b4d36f2646e221d8e2dfafd53
enaho murphy <enahomurphy@gmail.com>
2016-11-18 09:39:01 +0100

    chore(houndCi error) fix merge conflict

--- jasmine/spec/test.spec.js
+++ jasmine/spec/test.spec.js
@@ -2,11 +2,11 @@ function sqrt(x) {
   return x * x;
 };
 
-describe('simple test ' , function() {
 
+describe('simple test ' , function() {
   it('should return sqrt ', function() {
     var root = sqrt(4);
     expect(root).toEqual(16);
   });
-
 });
+
