9b749e70e8779360a1c7c2bc37bcf3dc8997017f
andela-sayidu <shalom.ayidu@andela.com>
2016-11-22 18:31:39 +0100

    feature(jwt_tokens): integrate signup with jwt token feature

---
+++ app/controllers/auth.js
@@ -0,0 +1,78 @@
+'use strict';
+
+let mongoose = require('mongoose');
+let jwt = require('jsonwebtoken');
+let config = require('../../config/config');
+let User = mongoose.model('User');
+
+
+let name = req.body.name;
+let username = req.body.username;
+let email = req.body.email;
+let pwd = req.body.password;
+let avatar = req.body.avatar;
+
+module.exports.signUp = (req, res) => {
+
+  User.findOne({
+    email: email
+  }, function (err, registeredUser) {
+    if (err) {
+      throw err;
+    }
+
+    if (!registeredUser) {
+      if (name && email && username && pwd) {
+        let user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = pwd;
+        user.avatar = avatar;
+
+        user.save({
+          function(err) {
+            if (err) { throw err; }
+            let jwttoken = jwt.sign(user, config.secret, { expiresInMinutes: 1440 });
+            res.json({ success: true, message: 'Thank you for signing up!.', token: token });
+          }
+        });
+      }
+    } else if (user) {
+      res.json({ message: 'Incomplete SignUp Details Provided.' });
+    } else {
+      res.json({ message: 'User already exists!' });
+    }
+  });
+};
+
+
+module.exports.login = (req, res) => {
+  User.findOne({
+    username: username
+  }, function (err, savedUser) {
+    if (err) {
+      res.send(err);
+    }
+
+    if (!savedUser) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (savedUser) {
+      if (savedUser.password != pwd) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440
+        });
+
+        res.json({
+          success: true,
+          message: 'Sucessful Login!',
+          token: token
+        });
+      }
+
+    }
+
+  })
+
+}
