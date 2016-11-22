ba549ee1eab4769262044cfaaa8fdd771a583e97
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-22 08:22:08 +0100

    change local port to 5000

--- config/env/all.js
+++ config/env/all.js
@@ -4,6 +4,7 @@ const path = require('path'),
 
 module.exports = {
   root: rootPath,
+<<<<<<< f8e3f415a8c266240efb7958a79e137aed03416f
   port: process.env.PORT || 3000,
   db: process.env.MONGOHQ_URL || 'mongodb://localhost/cfh'
 };
