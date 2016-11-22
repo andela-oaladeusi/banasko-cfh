ae41d8341e5e41b19f3390f36de4edff0ec1b85a
andela-sayidu <shalom.ayidu@andela.com>
2016-11-22 19:52:14 +0100

    feature(jwt_tokens): integrate signup with jwt token feature

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -6,13 +6,12 @@ let config = require('../../config/config');
 let User = mongoose.model('User');
 
 
-let name = req.body.name;
-let username = req.body.username;
-let email = req.body.email;
-let pwd = req.body.password;
-let avatar = req.body.avatar;
-
 module.exports.signUp = (req, res) => {
+  let name = req.body.name;
+  let username = req.body.username;
+  let email = req.body.email;
+  let pwd = req.body.password;
+  let avatar = req.body.avatar;
 
   User.findOne({
     email: email
@@ -20,35 +19,37 @@ module.exports.signUp = (req, res) => {
     if (err) {
       throw err;
     }
-
+    console.log(registeredUser)
     if (!registeredUser) {
-      if (name && email && username && pwd) {
+      if (name && email  && pwd) {
         let user = new User();
         user.name = name;
         user.email = email;
         user.password = pwd;
         user.avatar = avatar;
 
-        user.save({
-          function(err) {
+        user.save(function(err) {
             if (err) { throw err; }
-            let jwttoken = jwt.sign(user, config.secret, { expiresInMinutes: 1440 });
-            res.json({ success: true, message: 'Thank you for signing up!.', token: token });
-          }
-        });
+            let token = jwt.sign({user: user, iat: 1440 }, config.secret );
+            return res.json({ success: true, message: 'Thank you for signing up!.', token: token });
+          });
+      }else{
+         return res.json({ message: 'Incomplete SignUp Details Provided.' });
       }
-    } else if (user) {
-      res.json({ message: 'Incomplete SignUp Details Provided.' });
-    } else {
-      res.json({ message: 'User already exists!' });
-    }
-  });
-};
 
+    }else{
+      return res.json({ message: 'User already exists!' });
+    }
+
+  });
+}
 
 module.exports.login = (req, res) => {
+   let email = req.body.email;
+   let pwd = req.body.password;
+
   User.findOne({
-    username: username
+    email: email
   }, function (err, savedUser) {
     if (err) {
       res.send(err);
@@ -57,12 +58,10 @@ module.exports.login = (req, res) => {
     if (!savedUser) {
       res.json({ success: false, message: 'Authentication failed. User not found.' });
     } else if (savedUser) {
-      if (savedUser.password != pwd) {
+      if (!savedUser.authenticate(pwd)) {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
       } else {
-        var token = jwt.sign(user, config.secret, {
-          expiresInMinutes: 1440
-        });
+        var token = jwt.sign({email: email, iat: 1440 }, config.secret);
 
         res.json({
           success: true,
