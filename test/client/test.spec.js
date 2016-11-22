5bbf362f63734a7d420161aabddbdadcbaad5417
enaho murphy <murphy.enaho@andela.com>
2016-11-22 17:46:25 +0100

    refactor the test spec

--- test/client/test.spec.js
+++ test/client/test.spec.js
@@ -1,11 +1,10 @@
-
 function square(x) {
   return x * x;
 }
 
 describe('simple test ' , function() {
   it('should return sqrt ', function() {
-    const root = square(4);
-    expect(root).toEqual(16);
+    let sqr = square(4);
+    expect(sqr).toEqual(16);
   });
 })
