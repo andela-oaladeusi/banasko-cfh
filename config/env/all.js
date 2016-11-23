cd5ec47e86c484ab60a0be4576f90db66903288e
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-23 10:56:56 +0100

    Change mongoose_connection to mongooseConnection

--- config/env/all.js
+++ config/env/all.js
@@ -3,10 +3,10 @@
 var path = require('path'),
 rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
-// var testLocal = 'mongodb://localhost/cft';
+var testLocal = 'mongodb://localhost/cft';
 
 module.exports = {
   root: rootPath,
-  port: process.env.PORT,
-  db: process.env.MONGOHQ_URL
+  port: process.env.PORT || 4000,
+  db: process.env.MONGOHQ_URL ||testLocal
 };
--- config/express.js
+++ config/express.js
@@ -50,7 +50,8 @@ module.exports = function(app, passport, mongoose) {
             store: new mongoStore({
                 url: config.db,
                 collection: 'sessions',
-                mongoose_connection: mongoose.connection
+                // mongoose_connection: mongoose.connection
+                mongooseConnection: mongoose.connection
             })
         }));
 
