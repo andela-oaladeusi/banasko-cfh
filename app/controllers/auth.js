78d83cf8f1ed04379cb3a538701466129317645b
andela-sayidu <shalom.ayidu@andela.com>
2016-11-23 11:59:44 +0100

    feature(jwt_tokens):Address travis-ci build issues

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -7,7 +7,9 @@ const User = mongoose.model('User');
 
 exports.login = (req, res) => {
   const body = req.body;
-  User.findOne({ email: body.email }, function(err, user) {
+  User.findOne({
+    email: body.email
+  }, function (err, user) {
     if (err) {
       throw err;
     }
@@ -17,8 +19,11 @@ exports.login = (req, res) => {
         message: 'Invalid username or password'
       });
     }
-    const token = jwt.sign({user: user.email},
-      config.secret, { expiresIn: moment().add(7, 'd').valueOf()
+    const token = jwt.sign({
+        user: user.email
+      },
+      config.secret, {
+        expiresIn: moment().add(7, 'd').valueOf()
       });
     return res.status(200).json({
       success: true,
@@ -29,40 +34,58 @@ exports.login = (req, res) => {
 };
 
 exports.signUp = (req, res) => {
-
-  const name = req.body.name,
-    username = req.body.username,
-    email = req.body.email,
-    pwd = req.body.password,
-    avatar = req.body.avatar;
-
+  const name = req.body.name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const pwd = req.body.password;
+  const avatar = req.body.avatar;
 
   User.findOne({
     email: email
-  }, function (err, registeredUser) {
+  }, function (err, registerUser) {
     if (err) {
-      throw err;
+      res.send(err);
     }
-    console.log(registeredUser)
-    if (!registeredUser) {
-      if (name && email && pwd) {
-        const user = new User();
-        user.name = name;
-        user.email = email;
-        user.password = pwd;
-        user.avatar = avatar;
 
-        user.save(function (err) {
-          if (err) { throw err; }
-          const token = jwt.sign({ user: user, iat: 1440 }, config.secret);
-          return res.status(201).json({ success: true, message: 'Thank you for signing up!.', token: token });
-        });
+    if (!registerUser) {
+      if (name && email && pwd) {
+        if ((pwd.trim().length) < 8 || !validate.isEmail(email) ||
+          !validate.isAlpha(name) || !validate.isAlphanumeric(username)) {
+          res.status(400).json({
+            message: 'Invalid details provided.'
+          });
+        } else {
+          let user = new User();
+          user.name = name;
+          user.username = username;
+          user.email = email;
+          user.password = pwd;
+          user.avatar = avatar;
+
+          user.save((err, saveUser) => {
+            if (err) {
+              res.send(err);
+            }
+            let token = jwt.sign({
+              userId: saveUser._id,
+              expiresInMinutes: moments().day(7)
+            }, config.secret);
+            res.status(201).json({
+              success: true,
+              message: 'Thank you for signing up!',
+              token: token
+            });
+          });
+        }
       } else {
-        return res.status(400).json({ message: 'Incomplete SignUp Details Provided.' });
+        res.status(400).json({
+          message: 'Incomplete SignUp Details Provided.'
+        });
       }
     } else {
-      return res.status(409).json({ message: 'User already exists!' });
+      res.status(409).json({
+        message: 'User already exists!'
+      });
     }
   });
 };
-
