66c2d676f477281044dba47ff6e8b9c7290f295a
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-26 09:06:21 +0100

    Update socket.js

--- config/socket/socket.js
+++ config/socket/socket.js
@@ -106,7 +106,6 @@ module.exports = function (io) {
     }
   };
 
-	// for private in
   var getGame = function (player, socket, requestedGameId, createPrivate) {
     requestedGameId = requestedGameId || '';
     createPrivate = createPrivate || false;
