f4a8c98a17459c8532841511be2a6213af1640ea
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-29 08:52:16 +0100

    Add searchUser to routes.js

--- config/routes.js
+++ config/routes.js
@@ -95,4 +95,9 @@ module.exports = function (app, passport, auth) {
 
     //JWT Token (Signup and Login)
     app.post('/api/auth/login', jwtAuth.login);
+
+    // Search route
+    var search = require('../app/controllers/searchUser');
+    app.post('/api/search/users', search);
+
 };
