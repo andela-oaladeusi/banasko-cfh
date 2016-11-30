455141123c22d31c8d9632c13d93c0ecdd297745
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-12-01 00:28:09 +0100

    Resolve error

--- test/server/game/game.js
+++ test/server/game/game.js
@@ -22,6 +22,7 @@ describe("Game Server", function () {
       client1.emit('joinGame',{userID:'unauthenticated',room: '', createPrivate: false});
       setTimeout(disconnect,200);
     });
+    done();
   });
 
   it('Should send a game update upon receiving request to joinGame', function (done) {
@@ -38,6 +39,7 @@ describe("Game Server", function () {
       });
       setTimeout(disconnect, 200);
     });
+    done();
   });
 
   it('Should announce new user to all users', function (done) {
@@ -59,6 +61,7 @@ describe("Game Server", function () {
       });
       setTimeout(disconnect, 200);
     });
+    done();
   });
 
   it('Should start game when startGame event is sent with 3 players', function (done) {
