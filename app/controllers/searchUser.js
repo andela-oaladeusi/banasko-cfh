2d7bdcf41c44870a8cb71c885e28f744b614c682
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-11-30 23:19:49 +0100

    Add search api and updating files

--- app/controllers/searchUser.js
+++ app/controllers/searchUser.js
@@ -1,13 +1,12 @@
+'use strict';
 const User = require('../models/user');
 module.exports = (req, res) => {
-	User.find({ name: req.body.name }, (err , user) => {
-	if(err) {
-		return res.status(500).json(err.message);
-	}
-		if(user) {
-			return res.status(200).json({ success: true, email : user })
-		}else {
-			return res.status(400).json({ success: false, error : 'no user found' })
-		}
-	})
-}
+	let query = req.params.email;
+	User.find({ email: { $regex: query } }).limit(6)
+		.exec(function (err, user) {
+			if (err) {
+				return res.json(err);
+			}
+			return res.json(user);
+		});
+};
