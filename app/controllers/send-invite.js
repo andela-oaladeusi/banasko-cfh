5ab2928ee98b9c4364e79b67791006d35d0d2e11
andela-oaladeusi <olawale.aladeusi@andela.com>
2016-12-05 14:37:15 +0100

    Remove unnecessary console.log

--- app/controllers/send-invite.js
+++ app/controllers/send-invite.js
@@ -2,28 +2,22 @@
 const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 module.exports = (req, res) => {
-    console.log(req.body.email);
-    const smtpConfig = {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'test@test.com',
-            pass: 'test1234'
-        }
-    };
+	console.log(req.body.email);
+	const smtpConfig = {
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			user: 'plerootech@gmail.com',
+			pass: 'aladeusi'
+		}
+	};
 
-    let transporter = nodemailer.createTransport(smtpTransport(smtpConfig));
-    transporter.sendMail({
-        from: 'test@test.com',
-        to: req.body.email,
-        subject: 'Game Invitation!',
-        text: req.body.link
-    },  (error, response) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Message sent');
-        }
-    });
+	let transporter = nodemailer.createTransport(smtpTransport(smtpConfig));
+	transporter.sendMail({
+		from: 'test@test.com',
+		to: req.body.email,
+		subject: 'Game Invitation!',
+		text: req.body.link
+	});
 };
